b1816c8c42b22decd52d2e474281ea8807e67a56
Djouad SAHLI <sahlidjouad@gmail.com>
2024-10-23 15:04:44 +0100

    menu bar

--- _layouts/nav.js
+++ _layouts/nav.js
@@ -3,30 +3,21 @@ const nav = document.querySelector('nav');
 const navLinks = document.querySelector('.nav-links');
 
 navBtn.addEventListener('click', () => {
-  navLinks.classList.add('activated');
+  navLinks.classList.toggle('activated');
   const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
   navBtn.setAttribute('aria-expanded', !isExpanded);
-  !isExpanded && nav.classList.add('active');
-})
+});
 
-//INTERSECTION OBSERVER
-
-const navObs = new IntersectionObserver((entries) => nav.classList.toggle('active', !entries[0].isIntersecting)
-, {threshold: .85})
-
+const navObs = new IntersectionObserver((entries) => nav.classList.toggle('active', !entries[0].isIntersecting), { threshold: .85 });
 navObs.observe(document.querySelector('header'));
 
 const fadeUpObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
-    if(entry.isIntersecting){
+    if (entry.isIntersecting) {
       entry.target.classList.add('faded');
-      fadeUpObserver.unobserve(entry.target)
+      fadeUpObserver.unobserve(entry.target);
     }
-  })
-}, {
-  rootMargin: '-15%'
-})
+  });
+}, { rootMargin: '-15%' });
 
-document.querySelectorAll('.fade-up').forEach(el => {
-  fadeUpObserver.observe(el);
-})
+document.querySelectorAll('.fade-up').forEach(el => fadeUpObserver.observe(el));
